ef099540b876921a749da661a009927f60ef26a6
0x1 <kevin.francart3@gmail.com>
2026-07-30 01:12:54 +0200

    misc

--- src/Data/Lazy.php
+++ src/Data/Lazy.php
@@ -1,18 +1,31 @@
 <?php
 
+class Phpurs_Lazy {
+    private $f;
+    private $v;
+    private $done = false;
+    public function __construct($f) {
+        $this->f = $f;
+    }
+    public function __invoke(...$args) {
+        if ($this->done) return $this->v;
+        $this->v = ($this->f)(...$args);
+        $this->done = true;
+        return $this->v;
+    }
+}
+
 $exports['force'] = function($l) {
-    return $l(null);
+    $__num = \func_num_args();
+    $res = $l(null);
+    if ($__num > 1) {
+        return $res(...\array_slice(\func_get_args(), 1));
+    }
+    return $res;
 };
 
 $exports['defer'] = function($f) {
-    $v = null;
-    $done = false;
-    return function(...$args) use ($f, &$v, &$done) {
-        if ($done) return $v;
-        $v = $f(...$args);
-        $done = true;
-        return $v;
-    };
+    return new Phpurs_Lazy($f);
 };
 
 return $exports;
